a7e2bf24aa8b9f8e4cbba9f52bcf41cf8ff8f0e9
kanga-prog <9952@holbertonstudents.com>
2025-03-25 16:11:48 +0100

    Const or let?

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+export function taskFirst() {
+  const task = 'I prefer const when I can.'; // Utilisation de const
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let'; // Utilisation de let
+  combination += getLast(); // Modification de la variable
+
+  return combination;
+}
+
