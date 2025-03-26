40796c189e7ca86cd35e5a62b004ff3d09e88477
kanga-prog <9952@holbertonstudents.com>
2025-03-26 18:20:47 +0100

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false; // Utilisation de let pour la portée de bloc
-  const task2 = true; // Utilisation de let pour la portée de bloc
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
-    const task = true; // Nouvelle variable task dans le bloc conditionnel
-    const task2 = false; // Nouvelle variable task2 dans le bloc conditionnel
-  }
-
-  return [task, task2]; // Retour des variables
+  return [task, task2];
 }
