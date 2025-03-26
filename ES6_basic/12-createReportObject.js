3abd173b9f2360358960b4ce31fc0e6a3895b9e8
kanga-prog <9952@holbertonstudents.com>
2025-03-26 21:53:58 +0100

    Let's create a report object

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,  // assigne employeesList à la clé allEmployees
-    getNumberOfDepartments() {  // méthode pour compter le nombre de départements
-      return Object.keys(this.allEmployees).length;  // retourne le nombre de départements
-    }
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
   };
 }
-
