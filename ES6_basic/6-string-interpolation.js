55cdaa2d72ee0ec4e7297abd5369f9d0baa40d2b
kanga-prog <9952@holbertonstudents.com>
2025-03-26 20:16:31 +0100

    Take advantage of template literals

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,3 +1,4 @@
+// 6-string-interpolation.js
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
@@ -7,6 +8,6 @@ export default function getSanFranciscoDescription() {
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States, 
-    with a per capita personal income of ${budget.income}. As of 2015, San Francisco
-    proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+with a per capita personal income of ${budget.income}. As of 2015, San Francisco
+proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
