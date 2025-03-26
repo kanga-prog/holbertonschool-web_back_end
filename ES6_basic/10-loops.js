cd6453febe65a7ffc8140bb702426c9490425058
kanga-prog <9952@holbertonstudents.com>
2025-03-26 21:33:43 +0100

    For...of Loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value; // Modifie la valeur temporairement
-  }
-
-  return array.map(value => appendString + value); // Crée un nouveau tableau avec les valeurs modifiées
+  return array.map(value => appendString + value);
 }
