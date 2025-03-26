f25a4df875f89c033e6453eeab63772eb0199184
kanga-prog <9952@holbertonstudents.com>
2025-03-26 21:27:18 +0100

    ES6 method properties

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,18 +1,13 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
-      // Si income est déjà une chaîne avec un $, on ne l'ajoute pas de nouveau
-      if (income.startsWith('$')) {
-        return income; // Retourner directement la valeur si elle commence déjà par $
-      }
       return `$${income}`;
     },
     getIncomeInEuros(income) {
-      // Ajout d'un suffixe "euros" même si income commence déjà par un symbole monétaire
       return `${income} euros`;
     },
   };
