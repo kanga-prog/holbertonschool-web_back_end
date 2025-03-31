c1463fc466701a67b6cb551997f98eb7e1cc800e
kanga-prog <9952@holbertonstudents.com>
2025-04-01 00:03:34 +0200

    More map data structure

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceries.forEach((value, key) => {
+    if (value === 1) {
+      groceries.set(key, 100);
+    }
+  });
+}
+
+export default updateUniqueItems;
