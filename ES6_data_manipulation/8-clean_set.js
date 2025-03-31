1fefd8fa282c4aa2a827a8a1ad69e1ed5d08f65d
kanga-prog <9952@holbertonstudents.com>
2025-03-31 23:46:00 +0200

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const result = [...set]
     .filter((str) => str.startsWith(startString))
     .map((str) => str.slice(startString.length))
