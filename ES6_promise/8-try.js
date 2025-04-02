daa6c3651bbfa3c7ea135f1e58a3cf572432a10a
kanga-prog <9952@holbertonstudents.com>
2025-04-02 13:34:02 +0200

    Throw an error

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,11 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    // Vérification si le dénominateur est égal à zéro
-    if (denominator === 0 || numerator === 0 ) {
-      // Si le dénominateur est zéro, on lance une erreur
-      throw new Error('cannot divide by 0');
-    }
-  
-    // Si le dénominateur n'est pas zéro, on effectue la division
-    return numerator / denominator;
+  if (denominator === 0 || numerator === 0) {
+    throw new Error('cannot divide by 0');
   }
-  
+  return numerator / denominator;
+}
