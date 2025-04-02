296823bf20e08eff0157d9957dcf7fb0988b7a9d
kanga-prog <9952@holbertonstudents.com>
2025-04-02 13:15:33 +0200

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  })).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+  const fileData = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+  return Promise.resolve([userData, fileData]);
+}
