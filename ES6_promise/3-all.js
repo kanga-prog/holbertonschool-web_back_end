9ed14a22e3b812fb1cd10d5c5e1bcb5493e5c699
kanga-prog <9952@holbertonstudents.com>
2025-04-02 12:08:58 +0200

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  const newPromise = Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
+      return newPromise;
     });
 }
 
