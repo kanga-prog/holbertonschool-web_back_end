5e932a83d155e2093b83f401e4685bb10edb0edc
kanga-prog <9952@holbertonstudents.com>
2025-04-25 21:47:08 +0200

    Let's duck type an iterable object

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-import typing
 """
 Module for calculating the length of each element in a list of iterables.
 """
 
+from typing import Iterable, Sequence, List, Tuple
 
-def element_length(lst: typing.Iterable[typing.Sequence]) ->\
-      typing.List[typing.Tuple[typing.Sequence, int]]:
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-      Returns a list of tuples where each tuple contains\
-          an element from the input list
+    Returns a list of tuples where each tuple contains \
+        an element from the input list
     and its length.
 
     Args:
-        lst (Iterable[Sequence]): A list of iterable \
-            objects (such as strings, lists, etc.)
+        lst (Iterable[Sequence]): A list of iterable\
+              objects (such as strings, lists, etc.)
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples, each containing\
-              an element and its length.
+        List[Tuple[Sequence, int]]: A list of tuples, \
+            each containing an element and its length.
     """
     return [(i, len(i)) for i in lst]
