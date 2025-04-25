cadb9f952d46bd7d8f37f17b2fede3da3cda7d31
kanga-prog <9952@holbertonstudents.com>
2025-04-25 20:03:30 +0200

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module with type-annotated function `add`."""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two floats.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: Sum of a and b.
+    """
+    return a + b
