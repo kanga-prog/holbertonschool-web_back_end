3f494ef545ee5916f3805a973579529a54926949
kanga-prog <9952@holbertonstudents.com>
2025-04-25 23:42:06 +0200

    exo 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 """
-Module with a type-annotated function `wait_random` \
-    that waits for a random delay
-between 0 and a maximum value (max_delay) and returns it.
-
-The function is asynchronous and uses asyncio for managing the delay.
+Import wait_random from the previous python file that you have written
+and write an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. You will spawn wait_random n times
+with the specified max_delay.
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without
+using sort() because of concurrency.
 """
-import random
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous coroutine that waits for a random delay \
-        between 0 and max_delay (inclusive),
-    and then returns the delay.
-
-    Args:
-        max_delay (int): The maximum delay in seconds (default is 10).
-
-    Returns:
-        float: A random delay between 0 and max_delay seconds.
+    Let's execute multiple coroutines at the same time with async
     """
-    # Generate a random delay between 0 and max_delay
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    delays = []
+    coroutines = []
+    for number_coroutines in range(n):
+        coroutines.append(wait_random(max_delay))
+
+    for coroutine in asyncio.as_completed(coroutines):
+        # as_completed order coroutines by end of excecution (fastest first)
+        delay = await coroutine
+        delays.append(delay)
+
+    return delays
