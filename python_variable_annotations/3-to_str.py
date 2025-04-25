fcde229009aa4f54b6b42d4e1daab8c5397082d5
kanga-prog <9952@holbertonstudents.com>
2025-04-25 20:50:08 +0200

    to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provides a function to Convert a \
+    float to its string representation."""
+
+
 def to_str(n: float) -> str:
     """Convert a float to its string representation."""
     return str(n)
