8f52f2ce0cba2027af9d4bbd09730d034892a663
kanga-prog <9952@holbertonstudents.com>
2025-04-26 13:45:34 +0200

    Let's execute multiple coroutines at the same time with async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 """
-Module for executing multiple coroutines concurrently and returning their results in ascending order.
+Import wait_random from the previous python file that you have written
+and write an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. You will spawn wait_random n times
+with the specified max_delay.
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without
+using sort() because of concurrency.
 """
-
 import asyncio
-import importlib
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-# Import wait_random from '0-basic_async_syntax.py' using importlib
-wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous coroutine that runs the wait_random coroutine n times
-    with the specified max_delay.
-    It returns the list of delays in ascending order.
-
-    Args:
-        n (int): The number of times to run wait_random.
-        max_delay (int): The maximum delay value.
-
-    Returns:
-        list: List of delay times in ascending order.
+    Let's execute multiple coroutines at the same time with async
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
-    for task in asyncio.as_completed(tasks):
-        delay = await task
+    coroutines = []
+    for number_coroutines in range(n):
+        coroutines.append(wait_random(max_delay))
+
+    for coroutine in asyncio.as_completed(coroutines):
+        # as_completed order coroutines by end of excecution (fastest first)
+        delay = await coroutine
         delays.append(delay)
+
     return delays
