cec2bfbbfd5f17fa9eba1f52240c83b2afbaf2fa
kanga-prog <9952@holbertonstudents.com>
2025-05-02 16:22:57 +0200

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module to update topics of a school document based on the name."""
 
+
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name.
 
