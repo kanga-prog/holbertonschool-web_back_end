d63a01464089e8bccf34934c093de7ac9075b056
kanga-prog <9952@holbertonstudents.com>
2025-05-02 22:47:56 +0200

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""Module to update topics of a school document based on the name."""
+"""
+pymongo
+"""
 
 
 def update_topics(mongo_collection, name, topics):
-    """Changes all topics of a school document based on the name.
-
-    Args:
-        mongo_collection: pymongo collection object.
-        name (str): the name of the school to update.
-        topics (list): list of strings representing the topics.
-
-    Returns:
-        None
     """
-    mongo_collection.update_one(
-        {"name": name},  # Sélectionner le document par son nom
-        {"$set": {"topics": topics}}  # Mettre à jour le champ topics
-    )
+    function that changes all topics of a school
+    document based on the name
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
