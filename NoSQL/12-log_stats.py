25710d199d54b273091d3e779911579465af997e
kanga-prog <9952@holbertonstudents.com>
2025-05-02 22:51:38 +0200

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
-"""Module to display stats about Nginx logs."""
-
+"""
+Provides stats about Nginx logs stored in MongoDB.
+"""
 
 from pymongo import MongoClient
 
-def print_nginx_stats():
-    """Prints statistics from the Nginx logs collection."""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
 
-    # Get the total number of logs
+def log_stats():
+    """Displays statistics about Nginx logs."""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    # Total number of logs
     total_logs = collection.count_documents({})
-
-    # Print total logs
     print(f"{total_logs} logs")
 
-    # Methods count
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    # HTTP Methods count
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    # Count for method GET and path /status
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    # Number of GET requests to path "/status"
+    status_check = collection.count_documents({"method": "GET",
+                                               "path": "/status"})
     print(f"{status_check} status check")
 
-
 if __name__ == "__main__":
-    print_nginx_stats()
+    log_stats()
