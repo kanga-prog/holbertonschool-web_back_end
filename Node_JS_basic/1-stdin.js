ae1b661fcb5ab430ac7223d89b7e146904a6c27e
kanga-prog <9952@holbertonstudents.com>
2025-05-21 13:54:16 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,20 @@
+// Log first line
 console.log('Welcome to Holberton School, what is your name?');
-// await input
-process.stdin.resume();
-// once input is entered
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  // check if input is coming from the terminal
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
+// Start stdin process
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    // after collecting name from stdin, prints it
+    // null check for checker
+    if (name !== null) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  })
+  // when user exits program, log last statement
+
+  // this is where the child process is taken into account.
+  // the difference lies in when the program will end.
+  .on('end', () => {
     process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
-});
+  });
+// End of program!
