fe990f060d5f64b0e15a9f6c365c2211b5d5a2a3
kanga-prog <9952@holbertonstudents.com>
2025-05-22 14:57:36 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,9 @@
-// 1-stdin.js
-
-// Log first line
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// Start stdin process
 process.stdin.setEncoding('utf8');
-
-process.stdin
-  .on('readable', () => {
-    const input = process.stdin.read();
-    if (input !== null) {
-      const name = input.trim();
-      process.stdout.write(`Your name is: ${name}\n`);
-    }
-  })
-  .on('end', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
